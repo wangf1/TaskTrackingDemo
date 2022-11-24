308e8f50ea38dff7bb32c3a327087d537a3b0dff
Feng Wang <feng01.wang>
2022-11-24 21:18:24 +0800

    Project skeleton generated by Spring Initializr

---
+++ backend/src/main/java/com/wangf/tasktracking/TasktrackingApplication.java
@@ -0,0 +1,13 @@
+package com.wangf.tasktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktrackingApplication.class, args);
+	}
+
+}
