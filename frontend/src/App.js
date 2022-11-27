bf4021ea278a7b20257473ab14be307843842ac0
Feng Wang <feng01.wang>
2022-11-27 21:37:16 +0800

    Add Route support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,62 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Stack from "@mui/material/Stack";
 import Users from "./user/Users";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
+
+import Router1 from "./route/routes";
+
+export default function App() {
   return (
     <div>
-      <Users />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Users />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/projects" element={<NoMatch />} />
+
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <Stack spacing={2} direction="row">
+        {" "}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </Stack>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,11 +12,13 @@ const config = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   devServer: {
     open: true,
     host: "localhost",
     port: 3000,
+    historyApiFallback: true,
   },
   devtool: "eval-source-map",
   plugins: [
